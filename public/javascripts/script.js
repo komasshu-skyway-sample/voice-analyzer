ed68245f0157be251736d373d4808fceee3f3d47
kensakukomatsu <kensaku.komatsu@gmail.com>
2014-02-08 14:27:00 +0900

    for issue #1 delta analyzing

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -41,12 +41,15 @@ var VoiceAnalyzer = function(){
   this.SUM_THRESH = 5000;
 
   this.recordedDatas = []
+  this.prevFFT;
+  this.deltaFFT;
 };
 
 VoiceAnalyzer.prototype.start = function(stream){
   this.mediastreamsource = this.audioContext.createMediaStreamSource(stream);
   this.mediastreamsource.connect(this.analyzer);
   this.fftSize = this.analyzer.fftSize
+  // this.deltaFFT = new Uint8Array(this.fftSize / 2);
 
   this.getDatas();
   // this.analyzer.connect(this.audioContext.destination);
@@ -80,15 +83,33 @@ VoiceAnalyzer.prototype.stopRecording = function(){
   }
 }
 
+VoiceAnalyzer.prototype.calcDeltaFFT = function(fft){
+  var d_;
+  if(!!this.prevFFT === false) {
+    this.deltaFFT = new Uint8Array(this.fftSize / 2);
+  } else {
+    for(var i = 0, l = this.deltaFFT.length; i < l; i++) {
+      d_ = fft[i] - this.prevFFT[i];
+      this.deltaFFT[i] = d_ < 0 ? 0 : d_;
+    }
+  }
+  this.prevFFT = new Uint8Array(fft);
+}
+
+
 VoiceAnalyzer.prototype.getDatas = function(){
   var buffTimeDomain = new Uint8Array(this.fftSize)
     , buffFrequency = new Uint8Array(this.fftSize / 2)
   setInterval(function(ev){
     this.analyzer.getByteTimeDomainData(buffTimeDomain);
     this.analyzer.getByteFrequencyData(buffFrequency);
+    this.calcDeltaFFT(buffFrequency);
     var sum = this.getSum(buffTimeDomain);
     $("#sum").text(sum);
     drawPCM($("#pcm")[0], buffTimeDomain, "#00f");
+    if(this.deltaFFT) {
+      drawPCM($("#fft_delta")[0], this.deltaFFT, "#f00");
+    }
     drawPCM($("#fft")[0], buffFrequency, "#f00");
 
     if(this.flagRecording) this.recordDatas(buffTimeDomain, buffFrequency, sum);
@@ -107,29 +128,39 @@ VoiceAnalyzer.prototype.getSum = function(datas){
 
 
 VoiceAnalyzer.prototype.recordDatas = function(pcm, fft, sum){
-  var obj = {"pcm": pcm, "fft": fft, "sum": sum, "timestamp": new Date().toString()};
+  var obj = {
+    "pcm": pcm, 
+    "fft": fft, 
+    "fft_delta": new Uint8Array(this.deltaFFT), 
+    "sum": sum, 
+    "timestamp": new Date().toString()};
   this.recordedDatas.push(obj);
   this.appendGraph(obj);
 }
 
-VoiceAnalyzer.prototype.appendGraph = function(obj /* {"pcm", "fft", "sum", "timestamp"} */) {
+VoiceAnalyzer.prototype.appendGraph = function(obj /* {"pcm", "fft", "sum", "fft_delta", "timestamp"} */) {
   var $div = $("<div>");
   var $sum = $("<p>").append($("<span>").text("sum : "+obj.sum));
   var $fft = $("<canvas>").attr("class", "fft");
+  var $fft_delta = $("<canvas>").attr("class", "fft_delta");
   var $pcm = $("<canvas>").attr("class", "pcm");
 
   console.log(obj.sum);
 
   if(obj.sum > this.SUM_THRESH) {
     $fft.addClass("strong");
+    $fft_delta.addClass("strong");
   }
 
   $div
     .append($sum)
     .append($fft)
+    .append($fft_delta)
     .append($pcm)
     .appendTo("#recorded");
+
   drawPCM($fft[0], obj.fft, "#f00");
+  drawPCM($fft_delta[0], obj.fft_delta, "#f00");
   drawPCM($pcm[0], obj.pcm, "#00f");
 }
 
